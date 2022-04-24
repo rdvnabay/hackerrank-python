8b0e29705e14571c8f99889043a28ca6b2bc961c
rdvnabay <ridvanabay@gmail.com>
2022-04-24 14:13:22 +0300

    Solution 3: Python: Division

---
+++ python_division.py
@@ -0,0 +1,26 @@
+# Task
+
+"""
+The provided code stub reads two integers, a and b, from STDIN.
+Add logic to print two lines. 
+The first line should contain the result of integer division, a // b. 
+The second line should contain the result of float division, a / b.
+
+No rounding or formatting is necessary.
+"""
+
+# Input
+"""
+Input Format
+The first line contains the first integer, a.
+The second line contains the second integer, b.
+"""
+a = 4
+b = 3
+
+# Output
+"""
+Print the two lines as described above. 
+"""
+print(a//b) # 1
+print(a/b)  # 1.33...
