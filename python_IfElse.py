27770ccd5a4fac5489785a6391cd3542d4f6aaca
rdvnabay <ridvanabay@gmail.com>
2022-04-24 13:41:02 +0300

    Solution 1: Python If-Else

---
+++ python_IfElse.py
@@ -0,0 +1,13 @@
+# input
+n = 18
+
+# solution
+if(n % 2 != 0):
+    print("Weird")
+else:
+    if(n >= 2 and n <= 5):
+        print("Not Weird")
+    elif(n >= 6 and n <= 20):
+        print("Weird")
+    elif(n >= 20):
+        print("Not Weird")
